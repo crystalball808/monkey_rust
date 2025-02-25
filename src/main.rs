38ce3422b5b1d51dbd9a3d5581c9266db1659e47
Roman <romagritcaenko@gmail.com>
2025-02-25 14:41:03 +0200

    init cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Launch a REPL
+    Repl,
+    Run {
+        path: PathBuf,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    let Some(command) = cli.command else {
+        return;
+    };
+
+    match command {
+        Commands::Repl => {
+            println!("Repl todo")
+        }
+        Commands::Run { path } => {
+            let foo = path.into_os_string().into_string().unwrap();
+            println!("run {foo} todo");
+        }
+    }
 }
