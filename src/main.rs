61ebaa94a5015a2f328e33866a21fc0b97290086
Roman <romagritcaenko@gmail.com>
2025-03-02 17:12:55 +0200

    use lexer in repl

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use std::path::PathBuf;
+use std::{
+    io::{self, Write},
+    path::PathBuf,
+};
 
 use clap::{Parser, Subcommand};
+use monkey_rust::Lexer;
 
 #[derive(Parser)]
 struct Cli {
@@ -25,11 +29,30 @@ fn main() {
 
     match command {
         Commands::Repl => {
-            println!("Repl todo")
+            if let Err(error) = run_repl() {
+                eprintln!("REPL crashed: {error}")
+            }
         }
         Commands::Run { path } => {
             let foo = path.into_os_string().into_string().unwrap();
             println!("run {foo} todo");
         }
     }
+}
+
+fn run_repl() -> io::Result<()> {
+    let reader = io::stdin();
+
+    print!("Welcome to the Monkey REPL!\n>> ");
+    io::stdout().flush()?;
+    let mut input = String::new();
+    while let Ok(_bytes) = reader.read_line(&mut input) {
+        let tokens: Vec<_> = Lexer::new(&input).collect();
+
+        println!("{:?}", tokens);
+        input.clear();
+        print!(">> ")
+    }
+
+    Ok(())
 }
