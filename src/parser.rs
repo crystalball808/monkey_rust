8960397b226b74b6d0e5cded8ba0c54d66c16f5c
Roman <romagritcaenko@gmail.com>
2025-03-04 15:09:29 +0200

    parse let statements

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,7 @@
-use crate::{Lexer, ast::Program};
+use crate::{
+    Lexer, Token,
+    ast::{Expression, Program, Statement},
+};
 
 struct Parser<'a> {
     lexer: Lexer<'a>,
@@ -9,7 +12,43 @@ impl<'l> Parser<'l> {
         Self { lexer }
     }
     fn parse_program(self) -> Result<Program<'l>, String> {
-        todo!()
+        let mut lexer = self.lexer.peekable();
+
+        let mut statements: Vec<Statement> = Vec::new();
+        while let Some(token) = lexer.next() {
+            match token {
+                Token::Let => {
+                    let name = match lexer
+                        .next()
+                        .ok_or(String::from("Let statement not finished"))?
+                    {
+                        Token::Identifier(ident) => ident,
+                        _ => return Err(String::from("Illegal synthax")),
+                    };
+
+                    let Some(Token::Assign) = lexer.next() else {
+                        return Err(String::from("Let statement without the assign token"));
+                    };
+
+                    let expr = match lexer
+                        .next()
+                        .ok_or(String::from("Let statement not finished"))?
+                    {
+                        Token::Int(integer) => Expression::Int(integer),
+                        _ => return Err(String::from("Illegal synthax")),
+                    };
+
+                    statements.push(Statement::LetStatement(name, expr));
+
+                    let Some(Token::Semicolon) = lexer.next() else {
+                        return Err(String::from("Let statement without the semicolon"));
+                    };
+                }
+                _ => {}
+            }
+        }
+
+        Ok(Program::new(statements))
     }
 }
 
@@ -20,11 +59,13 @@ fn let_statement() {
     let input = "
 let x = 5;
 let y = 10;
-let foobar = 838383";
+let foobar = 838383;";
 
     let lexer = Lexer::new(input);
     let parser = Parser::new(lexer);
-    let parsed_ast = parser.parse_program().expect("Failed to parse the program");
+    let parsed_ast = parser
+        .parse_program()
+        .expect("Should be parsed successfully");
 
     let expected_ast = Program::new(vec![
         Statement::LetStatement("x", Expression::Int(5)),
